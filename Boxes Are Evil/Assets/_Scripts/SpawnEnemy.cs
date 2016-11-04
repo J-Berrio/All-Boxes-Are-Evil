b9149d9d37d8e7d6a5a4c4914c5ce1faf8bc1b83
Julia Berrio <berrioja@gmail.com>
2016-11-03 22:48:31 -0600

    Spawn Enemies

---
+++ Boxes Are Evil/Assets/_Scripts/SpawnEnemy.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    public Transform[] spawnLoc;
+    public GameObject[] enemies;
+    public float spawnFrequency = 1;
+    public bool canSpawnEnemy = true;
+
+    private int i = 0;
+    private int j = 0;
+	
+	void Start ()
+    {
+        StartCoroutine(SpawnEnemies()); 
+	}
+
+    IEnumerator SpawnEnemies ()
+    {
+        while(canSpawnEnemy)
+        {
+            i = Random.Range(0, spawnLoc.Length - 1);
+            enemies[j].SetActive(true);
+            enemies[j].transform.position = spawnLoc[i].position;
+
+            if(j < enemies.Length -1)
+            {
+                j++;
+            }
+            else
+            {
+                j = 0;
+            }
+
+            yield return new WaitForSeconds(spawnFrequency);
+        }
+    }
+}
