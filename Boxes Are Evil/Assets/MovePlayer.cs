93ee471553f658d42c7d9ea2c380a7c56ee05687
Julia Berrio <berrioja@gmail.com>
2016-11-03 21:07:15 -0600

    Move Player

---
+++ Boxes Are Evil/Assets/MovePlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float speed = 1;
+    private Vector3 playerPos;
+    private CharacterController controller;
+	
+	void Start ()
+    {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	
+	void Update ()
+    {
+        float horizontal = speed * Input.GetAxis("Horizontal");
+        float vertical = speed * Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0f, vertical);
+        controller.Move (movement * Time.deltaTime);
+	}
+}
