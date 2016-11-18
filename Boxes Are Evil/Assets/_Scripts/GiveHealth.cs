194799339e6345b2c4fc7dcfb97f6d3f8235ac5e
Julia Berrio <berrioja@gmail.com>
2016-11-17 19:18:14 -0700

    GreenBox adds health

---
+++ Boxes Are Evil/Assets/_Scripts/GiveHealth.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GiveHealth : MonoBehaviour
+{
+    public float giveHealth;
+    public GameObject player;
+
+    //just for giving health, there's only one box that does this
+    public void OnTriggerEnter(Collider player)
+    {
+        StaticVar.maxHealth = StaticVar.maxHealth + giveHealth;
+        print(StaticVar.maxHealth);
+
+        if(StaticVar.maxHealth > 100)
+        {
+            StaticVar.maxHealth = 100;
+        }
+    }
+}
