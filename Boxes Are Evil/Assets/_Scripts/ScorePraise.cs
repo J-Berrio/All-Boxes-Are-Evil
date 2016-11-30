675987709cd6128e23566f0b171b200cc01423a4
Julia Berrio <berrioja@gmail.com>
2016-11-29 22:07:24 -0700

    1stDelegate
    score milestone praise

---
+++ Boxes Are Evil/Assets/_Scripts/ScorePraise.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScorePraise : MonoBehaviour
+{
+    delegate void Multidelegate();
+    Multidelegate highScores;
+
+    //So one day, this will be rearranged so that in the game, when you
+    //reach 100 points, 200 points, etc you get PRAISE
+    //Right now its  set at 100 points because currently there's no way to gain
+    //points, so 100 seems pretty good
+    void Start()
+    {
+        highScores += FirstHighScore;
+        highScores += Fireworks;
+
+        if(highScores != null)
+        {
+            highScores();
+        }
+    }
+
+    void FirstHighScore()
+    {
+        print("Yay! You've reached 100 points!");
+    }
+
+    void Fireworks()
+    {
+        //some VFX crap that i dont have right now
+        print("VFX! Whooo! Pew pew pew pew! Whooooosh!");
+    }
+
+}
