b6f5bdc3656445b47d7c3566422ca96bc27e323a
Julia Berrio <berrioja@gmail.com>
2016-12-03 14:58:50 -0700

    Beginning Lists

---
+++ Boxes Are Evil/Assets/_Scripts/CollectItems.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectItems : MonoBehaviour
+{
+    public string itemName;
+    public int powerAttack;
+    
+
+    public CollectItems(string newName, int newPowerAttack)
+    {
+        itemName = newName;
+        powerAttack = newPowerAttack;
+    }
+}
+
---
+++ Boxes Are Evil/Assets/_Scripts/ListStuff.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListStuff : MonoBehaviour
+{
+    private void Start()
+    {
+        List<CollectItems> collectibles = new List<CollectItems>();
+
+        collectibles.Add(new CollectItems("Epic Axe of Headbashing", 150));
+        collectibles.Add(new CollectItems("Seriously Obnoxious Curse", 345));
+        collectibles.Add(new CollectItems("Forked Tongue", 75));
+
+    }
+
+  
+}
