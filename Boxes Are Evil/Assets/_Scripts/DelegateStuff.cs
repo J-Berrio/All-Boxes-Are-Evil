6270a8de059305e554a2e3a4b3703037026c6e6b
Julia Berrio <berrioja@gmail.com>
2016-12-04 16:58:34 -0700

    Second Delegate

---
+++ Boxes Are Evil/Assets/_Scripts/DelegateStuff.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DelegateStuff : MonoBehaviour
+{
+    public static Func<string> DelegateAction;
+    public string names;
+
+    void OnBecameVisible()
+    {
+        print(DelegateAction());
+    }
+}
---
+++ Boxes Are Evil/Assets/_Scripts/HandlingDelegates.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class HandlingDelegates : MonoBehaviour
+{
+    void Start()
+    {
+        DelegateStuff.DelegateAction += DelegateActHandler;
+    }
+
+    private string DelegateActHandler()
+    {
+        print("What's your name?");
+        return "George";
+    }
+}
