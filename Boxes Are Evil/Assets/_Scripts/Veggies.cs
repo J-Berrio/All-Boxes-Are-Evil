27483c195d470911156789cd3dcce2147493dfd6
Julia Berrio <berrioja@gmail.com>
2016-12-06 19:17:47 -0700

    Polymorphism stuff

---
+++ Boxes Are Evil/Assets/_Scripts/Veggies.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Veggies : MonoBehaviour
+{
+    public void Peel ()
+    {
+        print("Get your damn peeler, Steve, we're makin' soup.");
+    }
+
+    public void Chop ()
+    {
+        print("Where's the knife?");
+    }
+
+    public void SaladShooter()
+    {
+        print("It's this weird machine that slices and shoots out whatever vegetable you put inside");
+    }
+
+}
