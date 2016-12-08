5f55615f80f77cf62b25de939df4cf5b67abfd2f
Julia Berrio <berrioja@gmail.com>
2016-12-07 22:06:48 -0700

    Switch 2

---
+++ Boxes Are Evil/Assets/_Scripts/AnotherSwitch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnotherSwitch : MonoBehaviour
+{
+    public int animals = 3;
+
+    void OnTriggerEnter()
+    {
+        TheZoo();
+    }
+
+    public void TheZoo()
+    {
+        print("Enter");
+        switch(animals)
+        {
+            case 3:
+                print("The cow goes MOO");
+                goto case 2;
+
+            case 2:
+                print("The duck does QUACK");
+                goto case 1;
+
+            case 1:
+                print("What does the fox say?");
+                goto default;
+
+            default:
+                print("Muahahaha");
+                break;
+        }
+    }
+}
